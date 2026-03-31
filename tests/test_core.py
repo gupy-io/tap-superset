8d2e195cbf02d29d25d3bd8bdd27877683bb9fdd
Igor Sant'Ana <igor@alvadorn.com.br>
2026-03-31 17:27:46 -0300

    chore: update dependencies and Python version support
    * Upgrade Poetry version from 1.5.1 to 2.1.4
    * Update Python version requirement to >=3.10,<3.13
    * Modify dependencies for singer-sdk and other packages
    * Adjust test configurations to remove support for Python 3.8 and 3.9
    * Refactor test setup to skip if required environment variables are missing

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,22 +2,30 @@
 
 import datetime
 import os
+import pytest
 
 from singer_sdk.testing import get_tap_test_class
 
 from tap_superset.tap import TapSuperset
 
-SAMPLE_CONFIG = {
-    "start_date": (datetime.datetime.now() - datetime.timedelta(hours=6)).strftime(
-        "%Y-%m-%d %H:%M:%S"
-    ),
-    "username": os.environ.get("TAP_SUPERSET_USERNAME"),
-    "password": os.environ.get("TAP_SUPERSET_PASSWORD"),
-    "base_url": os.environ.get("TAP_SUPERSET_BASE_URL"),
-}
+username = os.environ.get("TAP_SUPERSET_USERNAME")
+password = os.environ.get("TAP_SUPERSET_PASSWORD")
+base_url = os.environ.get("TAP_SUPERSET_BASE_URL")
 
+if username and password and base_url:
+    SAMPLE_CONFIG = {
+        "start_date": (datetime.datetime.now() - datetime.timedelta(hours=6)).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        ),
+        "username": username,
+        "password": password,
+        "base_url": base_url,
+    }
 
-TestTapSuperset = get_tap_test_class(
-    tap_class=TapSuperset,
-    config=SAMPLE_CONFIG,
-)
+    TestTapSuperset = get_tap_test_class(
+        tap_class=TapSuperset,
+        config=SAMPLE_CONFIG,
+    )
+else:
+    # Skip tests if no config is provided
+    pytest.skip("Missing TAP_SUPERSET_USERNAME, TAP_SUPERSET_PASSWORD, or TAP_SUPERSET_BASE_URL", allow_module_level=True)
